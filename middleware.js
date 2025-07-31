5ea78b7309317a8b1840f62fb062d8bffdf0fee3
Himanshu Raj <himanshuraj48512@gmail.com>
2025-07-31 20:52:43 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,6 +1,9 @@
 // middleware.ts
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export function middleware() {
-  return new NextResponse('Server temporarily down', { status: 503 });
+export function middleware(request: NextRequest) {
+  return new NextResponse('403 Forbidden – Access Denied', {
+    status: 403,
+  });
 }
