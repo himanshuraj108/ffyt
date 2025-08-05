a12a27583c2fa38556ab1f8de9f0476c4dc10f82
Himanshu Raj <himanshuraj48512@gmail.com>
2025-08-05 21:04:59 +0530

    Update HomePage.jsx

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -46,7 +46,7 @@ const HomePage = () => {
               href="/status"
               className="inline-block bg-red-600 text-white px-6 py-3 rounded-full text-lg font-bold shadow-md hover:bg-blue-700 transform hover:scale-105 transition-all duration-300 animate-pulse"
             >
-              CHECK YOUR STATUS
+              CHECK YOUR UID STATUS
             </Link>
           </div>
         </div>
